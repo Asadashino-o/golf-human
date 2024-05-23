a26ada32871c2e50c22440ed4a4b2fdb7344fd20
Tingyu Xie <70675441+Asadashino-o@users.noreply.github.com>
2024-05-23 15:56:42 +0800

    Add files via upload

--- flaskk/app.py
+++ flaskk/app.py
@@ -4,16 +4,14 @@ import threading
 import sys
 import time
 from flask_cors import CORS
-
 from flask import Flask, render_template, request, redirect, abort, jsonify, send_file
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 sys.path.append('..')  # 将上级目录添加到Python路径中
-from merge2 import main as fct  # 替换 your_function 为你在 merge2.py 中想要导入的函数名或变量名
-from queue import Queue
+from merge2 import main as both
+from head_and_buttock import main as dtl
+from head import head as fo
 
-# 创建一个全局队列用于线程间通信
-message_queue = Queue()
 app = Flask(__name__)
 CORS(app)  # 允许跨域请求
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -28,7 +26,7 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 
 if not os.path.exists(app.config['PROCESSED_FOLDER']):
     os.makedirs(app.config['PROCESSED_FOLDER'])
-# 假设处理后的视频存储在项目根目录下的 processed_videos 目录中
+
 PROCESSED_VIDEO_DIR = os.path.join(os.getcwd(), 'processed')
 OUTPUT = os.path.join(os.getcwd(), 'processed/output')
 
@@ -42,7 +40,7 @@ def index():
     return render_template('index.html')
 
 
-def process_video(filepath, processed_filepath, process_id):
+def process_video(filepath, processed_filepath, process_id, detection_type):
     # 记录开始时间
     start_time = time.time()
     global number
@@ -55,7 +53,14 @@ def process_video(filepath, processed_filepath, process_id):
         print("提取的数字:", number)
     else:
         print("未找到数字")
-    fct(filepath, processed_filepath, number, 4)
+    # 根据检测类型选择不同的处理逻辑
+    if detection_type == 'Face-on':
+        fo(filepath, processed_filepath, number, 4)
+    elif detection_type == 'Down-the-line':
+        dtl(filepath, processed_filepath, number, 4)
+    elif detection_type == 'Both':
+        both(filepath, processed_filepath, number, 4)
+
     # 更新进度为100%
     progress_dict[process_id] = 100
     # 记录结束时间
@@ -64,16 +69,6 @@ def process_video(filepath, processed_filepath, process_id):
     runtime = end_time - start_time
 
     print("程序运行时间为：", runtime, "秒")
-    # message += "程序运行时间为：{} 秒".format(runtime)
-    # 将消息放入队列中
-    # message_queue.put(message)
-
-
-# @app.route('/get_message', methods=['GET'])
-# def get_message():
-#     # 从队列中获取消息，并返回给客户端
-#     message = message_queue.get()
-#     return jsonify({'message': message})
 
 
 @app.route('/upload', methods=['POST'])
@@ -88,13 +83,16 @@ def upload_file():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
 
-        # Process the video in a separate thread
+        detection_type = request.form.get('detection')
+        if detection_type not in ['Face-on', 'Down-the-line', 'Both']:
+            return jsonify({'error': 'Invalid detection type'}), 400
+
         processed_filename = f"processed_{filename}"
         processed_filepath = os.path.join(app.config['PROCESSED_FOLDER'], processed_filename)
         process_id = filename
 
         progress_dict[process_id] = 0
-        thread = threading.Thread(target=process_video, args=(filepath, processed_filepath, process_id))
+        thread = threading.Thread(target=process_video, args=(filepath, processed_filepath, process_id, detection_type))
         thread.start()
 
         return jsonify({'process_id': process_id, 'filename': processed_filename})
@@ -114,29 +112,20 @@ def processed(filename):
     file_path = os.path.join(PROCESSED_VIDEO_DIR, filename)
     output = os.path.join(OUTPUT, filename)
     try:
-        # 检查文件是否存在
         if os.path.exists(file_path):
-            # 使用 send_file 发送文件
             reduce_bitrate(file_path, output)
-            return send_file(output, mimetype='video/mp4')
+            return send_file(output, mimetype='video/mp4')  # 使用 send_file 发送文件
         else:
-            # 文件不存在，返回404错误
             return abort(404, description="File not found")
     except Exception as e:
-        # 处理其他可能的异常
         return abort(500, description=str(e))
 
 
 def reduce_bitrate(input_video, output_video):
-    # 读取输入视频文件
     video_clip = VideoFileClip(input_video)
-
-    # 改为h264
-    video_clip.write_videofile(output_video, codec="libx264")
-
-    # 关闭视频文件对象
+    video_clip.write_videofile(output_video, codec="libx264")  # 将视频编码改为h264，便于在页面展示
     video_clip.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2348, debug=True)
+    app.run(host='0.0.0.0', port=2348, debug=True) # 这是服务器的端口设置，目前是全开放端口,仅供测试并不安全，运行后的网址是{your_server_name}:2348
